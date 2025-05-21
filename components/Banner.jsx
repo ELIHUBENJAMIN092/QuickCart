f3a98932020c54073463fa9c449fd98fb6f9e770
ELIHUBENJAMIN092 <bquistial092@gmail.com>
2025-05-21 11:58:10 -0500

    nuevo commit

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between md:pl-20 py-14 md:py-0 bg-[#E6E9F2] my-16 rounded-xl overflow-hidden">
-      <Image
+    //<div className="flex flex-col md:flex-row items-center justify-between md:pl-20 py-14 md:py-0 bg-[#E6E9F2] my-16 rounded-xl overflow-hidden">
+    <div className="flex flex-col md:flex-row items-center justify-between md:pl-20 py-14 md:py-0 bg-[#E6E9F2] my-16 rounded-xl overflow-hidden h-[220px]">
+    <Image
         className="max-w-56"
         src={assets.jbl_soundbox_image}
         alt="jbl_soundbox_image"
