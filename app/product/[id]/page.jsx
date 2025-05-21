9e3957ceab6a0e0b315a8f59b5e1d3c1494b92fe
ELIHUBENJAMIN092 <bquistial092@gmail.com>
2025-05-21 11:28:17 -0500

    nuevo commit

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -95,15 +95,15 @@ const Product = () => {
                         <table className="table-auto border-collapse w-full max-w-72">
                             <tbody>
                                 <tr>
-                                    <td className="text-gray-600 font-medium">Brand</td>
-                                    <td className="text-gray-800/50 ">Generic</td>
+                                    <td className="text-gray-600 font-medium">Marca</td>
+                                    <td className="text-gray-800/50 ">Zebra</td>
                                 </tr>
                                 <tr>
                                     <td className="text-gray-600 font-medium">Color</td>
-                                    <td className="text-gray-800/50 ">Multi</td>
+                                    <td className="text-gray-800/50 ">Blanco</td>
                                 </tr>
                                 <tr>
-                                    <td className="text-gray-600 font-medium">Category</td>
+                                    <td className="text-gray-600 font-medium">Categoria</td>
                                     <td className="text-gray-800/50">
                                         {productData.category}
                                     </td>
@@ -124,7 +124,7 @@ const Product = () => {
             </div>
             <div className="flex flex-col items-center">
                 <div className="flex flex-col items-center mb-4 mt-16">
-                    <p className="text-3xl font-medium">Featured <span className="font-medium text-blue-600">Products</span></p>
+                    <p className="text-3xl font-medium">Productos <span className="font-medium text-blue-600">Destacados</span></p>
                     <div className="w-28 h-0.5 bg-blue-600 mt-2"></div>
                 </div>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-6 pb-14 w-full">
--- components/Banner.jsx
+++ components/Banner.jsx
@@ -12,10 +12,10 @@ const Banner = () => {
       />
       <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
         <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
-          Level Up Your Gaming Experience
+          Todo lo que buscas, para tu negocio
         </h2>
         <p className="max-w-[343px] font-medium text-gray-800/60">
-          From immersive sound to precise controls—everything you need to win
+          en un solo lugar con garantía y soporte garantizado.
         </p>
         <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-blue-600 rounded text-white">
           Comprar ahora
@@ -23,7 +23,7 @@ const Banner = () => {
         </button>
       </div>
       <Image
-        className="hidden md:block max-w-80"
+        className="hidden md:block max-w-56"
         src={assets.md_controller_image}
         alt="md_controller_image"
       />
