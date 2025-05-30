610be0bb1160c6d87b281d81586c3887d9b21d54
BENJAMIN <148173388+ELIHUBENJAMIN092@users.noreply.github.com>
2025-05-30 11:57:06 -0500

    nuevo commit

--- .expo/tienda-compel-movil/App.js
+++ .expo/tienda-compel-movil/App.js
@@ -2,16 +2,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
+import ClerkWithProvider from './ClerkProvider'; // 👈 asegúrate de importar esto correctamente
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Inicio">
-        <Stack.Screen name="Inicio" component={HomeScreen} />
-        <Stack.Screen name="Carrito" component={CartScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ClerkWithProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Inicio">
+          <Stack.Screen name="Inicio" component={HomeScreen} />
+          <Stack.Screen name="Carrito" component={CartScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ClerkWithProvider>
   );
 }
