4e411bb6213856785021cfe12136c2db11893135
BENJAMIN <148173388+ELIHUBENJAMIN092@users.noreply.github.com>
2025-06-21 22:09:25 -0500

    Eliminar

---
+++ app/api/product/delete/[id]/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+import authSeller from "@/lib/authSeller";
+import connectDB from "@/config/db";
+import Product from "@/models/Product";
+
+export async function DELETE(request, { params }) {
+  try {
+    const { userId } = getAuth(request);
+    const isSeller = await authSeller(userId);
+
+    if (!isSeller) {
+      return NextResponse.json({ success: false, message: "Not authorized" });
+    }
+
+    await connectDB();
+    const deletedProduct = await Product.findOneAndDelete({ _id: params.id, userId });
+
+    if (!deletedProduct) {
+      return NextResponse.json({ success: false, message: "Producto no encontrado" });
+    }
+
+    return NextResponse.json({ success: true, message: "Producto eliminado" });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
+}
