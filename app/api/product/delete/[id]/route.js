a38f8a2d78b7796b3169f5df4ea7ab53350c08fb
BENJAMIN <148173388+ELIHUBENJAMIN092@users.noreply.github.com>
2025-06-21 22:16:26 -0500

    Eliminar2

--- app/api/product/delete/[id]/route.js
+++ app/api/product/delete/[id]/route.js
@@ -4,17 +4,22 @@ import authSeller from "@/lib/authSeller";
 import connectDB from "@/config/db";
 import Product from "@/models/Product";
 
-export async function DELETE(request, { params }) {
+export async function DELETE(request, context) {
   try {
+    const { params } = context;
     const { userId } = getAuth(request);
     const isSeller = await authSeller(userId);
 
     if (!isSeller) {
-      return NextResponse.json({ success: false, message: "Not authorized" });
+      return NextResponse.json({ success: false, message: "No autorizado" });
     }
 
     await connectDB();
-    const deletedProduct = await Product.findOneAndDelete({ _id: params.id, userId });
+
+    const deletedProduct = await Product.findOneAndDelete({
+      _id: params.id,
+      userId,
+    });
 
     if (!deletedProduct) {
       return NextResponse.json({ success: false, message: "Producto no encontrado" });
