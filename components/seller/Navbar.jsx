4af9c7e1bce6cbad0a75ff6035d27ea8f5731c9e
ELIHUBENJAMIN092 <bquistial092@gmail.com>
2025-09-22 13:35:59 -0500

    Boton Salir 22092025

--- components/seller/Navbar.jsx
+++ components/seller/Navbar.jsx
@@ -4,13 +4,24 @@ import Image from 'next/image'
 import { useAppContext } from '@/context/AppContext'
 
 const Navbar = () => {
-
   const { router } = useAppContext()
 
   return (
     <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b'>
-      <Image onClick={()=>router.push('/')} className='w-28 lg:w-32 cursor-pointer' src={assets.logo} alt="" />
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Salir</button>
+      <Image 
+        onClick={() => router.push('/')} 
+        className='w-28 lg:w-32 cursor-pointer' 
+        src={assets.logo} 
+        alt="logo" 
+      />
+
+      {/* Botón para salir y redirigir al inicio */}
+      <button 
+        onClick={() => router.push('/')}
+        className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'
+      >
+        Salir
+      </button>
     </div>
   )
 }
