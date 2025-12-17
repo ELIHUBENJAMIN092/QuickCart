f3b4a30219259706a7e65e0e0a0a9b62d7629bee
ELIHUBENJAMIN092 <bquistial092@gmail.com>
2025-12-17 13:32:47 -0500

    Cambio en banner oferta 17122025

--- components/BannerOferta.jsx
+++ components/BannerOferta.jsx
@@ -14,7 +14,7 @@ export default function BannerOferta() {
   if (!isOpen) return null;
 
   return (
-    <div className="w-full bg-gradient-to-r from-blue-800 to-cyan-400 text-white text-center py-2 px-4">
+    <div className="w-full bg-gradient-to-r from-blue-600 to-blue-900 text-white text-center py-2 px-4">
       <div className="flex items-center justify-between max-w-7xl mx-auto">
         {/* Texto de la oferta */}
         <p className="font-medium text-sm">
@@ -25,7 +25,7 @@ export default function BannerOferta() {
           {/* Botón de copiar cupón */}
           <button
             onClick={handleClaim}
-            className="bg-white text-purple-700 font-semibold px-3 py-1 rounded hover:bg-purple-100 transition"
+            className="bg-white text-black font-medium px-3 py-1 rounded"
           >
             Copiar cupón
           </button>
